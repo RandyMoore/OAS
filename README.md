717a16b863ae4252bde8dc4d06115fb28a75920d
Randy <randal.a.moore@gmail.com>
2019-12-01 13:49:32 +0100

    Update README and demo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # OAS
 Open API Specification example
+
+Requirements:
+* docker
+* Node (preferably via nvm)
+
+Run `./run_me.sh`
+
+This will use openapi/oas.yaml to:
+* Generate javascript SDK code at openapi/javascript
+* Generate python server code at openapi/python_flask
+* Bundle a javascript example usage of of the generated javascript SDK (static/demo.js)
+* Bring up the Flask server serving the oas.yaml and the demo at http://localhost/example.html
+* Additionally, documentation for oas.yaml served by Swagger UI will be available at http://localhost:8080
--- static/demo.js
+++ static/demo.js
@@ -1,4 +1,5 @@
 import DefaultApi from './api/DefaultApi'
+import Band from './model/Band'
 
 let body = document.body
 body.innerHTML += 'Calling the API...<br/>'
@@ -6,9 +7,15 @@ body.innerHTML += 'Calling the API...<br/>'
 function handle(error, data, response) {
     body.innerHTML += 'Response code: ' + response.status + '<br/>'
     body.innerHTML += 'The bands we got back:'
-    data.map(band => {
-      const jsonText = JSON.stringify(band, null, 2) + '\n'
+    data.map(bandJSON => {
+      const jsonText = JSON.stringify(bandJSON, null, 2) + '\n'
       body.innerHTML += '<hr/> <pre>' + jsonText + '</pre>'
+    })
+    body.innerHTML += '<hr/> Creating Band objects from the JSON...' + '<br/>'
+    data.map(bandJSON => {
+      const band = Band.constructFromObject(bandJSON)
+      // Note use of dot notation to access elements, band is a class instance of Band
+      body.innerHTML += 'Name of the band is ' + band.name + '<br/>'
     })
 }
 
